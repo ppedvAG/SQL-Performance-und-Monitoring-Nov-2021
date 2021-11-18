2aac766ee353765d21f99d9587194304eee6efef
Rauchendraser <andreasr@ppedv.de>
2021-11-18 10:51:10 +0100

    Virtualsierung Setup

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# SQL Performance und Monitoring Nov 2021
+## SQL Performance und Monitoring Nov 2021
+
+# Tag 1
+
+* Virtualisierung
+* Setup aus Sicht der Perf
+* Serversettings
+* DB Settings / DB Design
+* Objektkunde:
+
+# Tag 2
  
+
+
+ Fumus ist der Andreas Rauch ;-) 
+
+ Fumus [lat: für Rauch]
---
+++ SQL Perfomance und Monitoring/SQL Perfomance und Monitoring/02_Setup.sql
@@ -0,0 +1,67 @@
+/*
+
+RAM
+Das OS berücksichtigen
+mind 4 GB... allerdings zuviel macht wenig Sinn
+
+Install-Pfad ..egal
+
+
+ServiceKonten
+NT Service\....  lok Konten --> \\Freigabe  (Computerkonto)
+-->autom Kennwortänderungen
+NT Service wird immer! für lokale Zugriffe verwendet
+Im Fall von DomKonto --> nur für Remote.. lokal immer noch NT Service
+
+MAXDOP 
+= Anzahl der Kerne max 8
+wieviel Kerne verwendet eine Abfrage maximal ... 
+
+
+Volumewartungstask: 
+Lok Sicherheitsrichtlinie
+Einem guten Admin ist ds rel Wurscht
+Dateivergrößerungen der DAtendateien werden nicht mehr vorher "ausgenullt"
+----------------------
+111111111111111110?0?0?
+----------------------
+
+
+DB Pfade
+Datendateien und Logdateien
+trenne die beiden physikalisch
+
+
+
+TempDB 
+was kommt dor rein....?
+#tab temporäre Tabelle
+##tab
+select .. order by .. Auslagerungen
+Zeilenversionierung
+IX Rebuilds
+eig HDDS für Log und Daten
+Anzahl der Dateien = Anzahl der Kerne (Max 8)
+TRaceflags
+T 1117   T 1118
+immer gleich große DB DatenDateien
+DS kommen in Seiten .. Seiten werden in Blöcken (8 Seiten)
+--keine mixed extents  sondern extents
+
+
+
+
+
+
+
+
+
+CPU  MAXDOP
+
+
+schulung\Administrator
+ppedv2019!
+
+
+
+*/
